f9a69f33f6231535c8fd220d4e30de58442ad796
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-26 12:05:18 -0500

    Actualizando explorers (PUT)

--- express_api/app.js
+++ express_api/app.js
@@ -34,3 +34,10 @@ app.post('/v1/explorers', (req, res) => {
 })
 
 // PUT
+app.put('/v1/explorers/:id', (req, res) => {
+  console.log(`API Explorers PUT request ${new Date()}`)
+  console.log(`Update explorer with id ${req.params.id}`)
+  const requestBody = req.body 
+  res.status(200).json({message: "Updated"})
+})
+
