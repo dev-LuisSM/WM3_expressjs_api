cfd48fc52ce150a02e3c2b4fa6d708c110c64fdf
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-26 12:06:26 -0500

    Eliminando explorers (DELETE)

--- express_api/app.js
+++ express_api/app.js
@@ -9,12 +9,12 @@ app.listen(port, () => {
 
 // GET
 
-app.get('/v1/explorers', (req, res) =>{
+app.get('/v1/explorers', (req, res) => {
   console.log(`API Explorers GET ALL request ${new Date()}`)
-  const explorer1 = {id: 1, name: "Luis"}
-  const explorer2 = {id: 2, name: "Luis2"}
-  const explorer3 = {id: 3 , name: "Luis3"}
-  const explorer4 = {id: 4, name: "Luis4"}
+  const explorer1 = { id: 1, name: "Luis" }
+  const explorer2 = { id: 2, name: "Luis2" }
+  const explorer3 = { id: 3, name: "Luis3" }
+  const explorer4 = { id: 4, name: "Luis4" }
   const explorers = [explorer1, explorer2, explorer3, explorer4]
   res.status(200).json(explorers)
 })
@@ -30,14 +30,21 @@ app.get('/v1/explorers/:id', (req, res) => {
 app.post('/v1/explorers', (req, res) => {
   console.log(`Api explorers POST request ${new Date()}`)
   const requestBody = req.body // Parámetros de un cliente
-  res.status(201).json({message: "Created"}) // 201 para post
+  res.status(201).json({ message: "Created" }) // 201 para post
 })
 
 // PUT
 app.put('/v1/explorers/:id', (req, res) => {
   console.log(`API Explorers PUT request ${new Date()}`)
   console.log(`Update explorer with id ${req.params.id}`)
-  const requestBody = req.body 
-  res.status(200).json({message: "Updated"})
+  const requestBody = req.body
+  res.status(200).json({ message: "Updated" })
 })
 
+// DELETE
+app.delete('/v1/explorers/:id', (req, res) => {
+  console.log(`API Explorers DELETE request ${new Date()}`)
+  console.log(`Delete explorer with id ${req.params.id}`)
+  const requestBody = req.body
+  res.status(200).json({ message: "Deleted" })
+})
