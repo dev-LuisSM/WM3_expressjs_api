57a9b3e6069710d445d793b6ec85b633a27dcb81
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-26 12:02:02 -0500

    Creando explorers (POST)

--- express_api/app.js
+++ express_api/app.js
@@ -7,8 +7,10 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+// GET
+
 app.get('/v1/explorers', (req, res) =>{
-  console.log(`API Explorers GET ALL request ${new Date().toDateString()}`)
+  console.log(`API Explorers GET ALL request ${new Date()}`)
   const explorer1 = {id: 1, name: "Luis"}
   const explorer2 = {id: 2, name: "Luis2"}
   const explorer3 = {id: 3 , name: "Luis3"}
@@ -18,8 +20,17 @@ app.get('/v1/explorers', (req, res) =>{
 })
 
 app.get('/v1/explorers/:id', (req, res) => {
-  console.log(`API Explorers GET request ${new Date().toDateString()}`)
+  console.log(`API Explorers GET request ${new Date()}`)
   console.log(`Getting explorer with id ${req.params.id}`)
   const explorer = { id: 1, name: "Luis" }
   res.status(200).json(explorer)
 })
+
+// POST
+app.post('/v1/explorers', (req, res) => {
+  console.log(`Api explorers POST request ${new Date()}`)
+  const requestBody = req.body // Parámetros de un cliente
+  res.status(201).json({message: "Created"}) // 201 para post
+})
+
+// PUT
