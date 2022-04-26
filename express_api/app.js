f680a0777b9682f710ed13b58e8b123ceab69e05
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-26 11:55:18 -0500

    Primer endpoint en ExpressJs

--- express_api/app.js
+++ express_api/app.js
@@ -1,1 +1,18 @@
+const express = require('express')
+const app = express()
+app.use(express.json()) // Indica que usamos JSON
+const port = 3000
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/v1/explorers', (req, res) =>{
+  console.log(`API Explorers GET ALL request ${new Date()}`)
+  const explorer1 = {id: 1, name: "Luis"}
+  const explorer2 = {id: 2, name: "Luis2"}
+  const explorer3 = {id: 3 , name: "Luis3"}
+  const explorer4 = {id: 4, name: "Luis4"}
+  const explorers = [explorer1, explorer2, explorer3, explorer4]
+  res.status(200).json(explorers)
+})
