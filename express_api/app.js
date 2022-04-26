e6b67a465a3b3284b8806a2c641c9ae8df79ae5b
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-26 11:58:29 -0500

    Obteniendo explorer con id

--- express_api/app.js
+++ express_api/app.js
@@ -8,11 +8,18 @@ app.listen(port, () => {
 })
 
 app.get('/v1/explorers', (req, res) =>{
-  console.log(`API Explorers GET ALL request ${new Date()}`)
+  console.log(`API Explorers GET ALL request ${new Date().toDateString()}`)
   const explorer1 = {id: 1, name: "Luis"}
   const explorer2 = {id: 2, name: "Luis2"}
   const explorer3 = {id: 3 , name: "Luis3"}
   const explorer4 = {id: 4, name: "Luis4"}
   const explorers = [explorer1, explorer2, explorer3, explorer4]
   res.status(200).json(explorers)
+})
+
+app.get('/v1/explorers/:id', (req, res) => {
+  console.log(`API Explorers GET request ${new Date().toDateString()}`)
+  console.log(`Getting explorer with id ${req.params.id}`)
+  const explorer = { id: 1, name: "Luis" }
+  res.status(200).json(explorer)
 })
